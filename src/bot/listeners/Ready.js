8aa0b30ef62a2e699eb4a6107ae4ac45fadfb428
Nak <14081562+Nak-Nic@users.noreply.github.com>
2020-12-21 21:07:43 -0600

    commit on Nick_Testing

--- src/bot/listeners/Ready.js
+++ src/bot/listeners/Ready.js
@@ -1,5 +1,5 @@
 const { Listener } = require('discord-akairo');
-
+//This is the recommended way of doing a listener
 class ReadyListener extends Listener {
     constructor() {
         super('ready', {
