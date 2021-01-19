c918ad50633018a959f740b9bee30fd25ff3cc48
George <georgej1088@gmail.com>
2021-01-19 16:12:36 -0500

    Run prettier

--- bin/bot.js
+++ bin/bot.js
@@ -1,23 +1,26 @@
-const { AkairoClient, CommandHandler } = require('discord-akairo');
-const path = require('path');
+const { AkairoClient, CommandHandler } = require("discord-akairo");
+const path = require("path");
 
-require('dotenv').config();
+require("dotenv").config();
 
 class NCEBot extends AkairoClient {
-    constructor() {
-        super({
-            // Options for Akairo go here.
-        }, {
-            // Options for discord.js goes here.
-        });
+  constructor() {
+    super(
+      {
+        // Options for Akairo go here.
+      },
+      {
+        // Options for discord.js goes here.
+      }
+    );
 
-        this.commandHandler = new CommandHandler(this, {
-            directory: path.join(__dirname, '../src/bot/commands/'),
-            prefix: 'nce',
-            // Options for the command handler goes here.
-        });
-        this.commandHandler.loadAll();
-    }
+    this.commandHandler = new CommandHandler(this, {
+      directory: path.join(__dirname, "../src/bot/commands/"),
+      prefix: "nce",
+      // Options for the command handler goes here.
+    });
+    this.commandHandler.loadAll();
+  }
 }
 
 const client = new NCEBot();
