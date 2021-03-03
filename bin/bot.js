8871442ba384f60e864b3152ab94486023bea4bb
Nak <14081562+Nak-Nic@users.noreply.github.com>
2021-03-03 16:59:10 -0600

    updated code to include mongoose changes

--- bin/bot.js
+++ bin/bot.js
@@ -1,27 +1,37 @@
-const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo');
-const path = require('path');
-require('dotenv').config();
+const { AkairoClient, CommandHandler } = require("discord-akairo");
+const path = require("path");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 class NCEBot extends AkairoClient {
-    constructor() {
-        super({
-            // Options for Akairo go here.
-        }, {
-            // Options for discord.js goes here.
-        });
-
-        this.commandHandler = new CommandHandler(this, {
-            directory: path.join(__dirname, '../src/bot/commands/'),
-            prefix: 'nce',
-            // Options for the command handler goes here.
-        });
-        this.listenerHandler = new ListenerHandler(this, {
-            directory: path.join(__dirname, '../src/bot/listeners/' )
-        })
-        this.commandHandler.loadAll();
-        this.commandHandler.useListenerHandler(this.listenerHandler);
-        this.listenerHandler.loadAll();
-    }
+  constructor() {
+    super(
+      {
+        // Options for Akairo go here.
+      },
+      {
+        // Options for discord.js goes here.
+      }
+    );
+    
+    this.commandHandler = new CommandHandler(this, {
+      directory: path.join(__dirname, "../src/bot/commands/"),
+      prefix: "nce",
+      // Options for the command handler goes here.
+    });
+    this.listenerHandler = new ListenerHandler(this, {
+      directory: path.join(__dirname, '../src/bot/listeners/' )
+    })
+    this.commandHandler.loadAll();
+    this.commandHandler.useListenerHandler(this.listenerHandler);
+    this.listenerHandler.loadAll();
+    this.db = mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+  }
 }
 
 
